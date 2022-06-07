c34e9767bae60c3b6dc6c6c1afb7b921e5f29ca8
Wisanu <wisanu2308@gmail.com>
2022-06-07 15:39:07 +0700

    close on submit

--- components/RegisterForm.vue
+++ components/RegisterForm.vue
@@ -54,21 +54,23 @@ export default {
   methods: {
     submitForm() {
 
-      if (confirm("ต้องการบันทึกข้อมูล?",true,'TEST')) {
-        // console.log(this.txtName)
-        // console.log(this.txtPhone)
+      console.log(liff.getVersion())
+      liff.closeWindow()
+      // if (confirm("ต้องการบันทึกข้อมูล?",true,'TEST')) {
+      //   // console.log(this.txtName)
+      //   // console.log(this.txtPhone)
 
-        liff.sendMessages([
-          {
-            type: "text",
-            text: "Hello, World!",
-          },
-        ]).then(() => {
-          console.log("message sent");
-        }).catch((err) => {
-          console.log("error", err);
-        });
-      }
+      //   liff.sendMessages([
+      //     {
+      //       type: "text",
+      //       text: "Hello, World!",
+      //     },
+      //   ]).then(() => {
+      //     console.log("message sent");
+      //   }).catch((err) => {
+      //     console.log("error", err);
+      //   });
+      // }
     }
   }
 }
