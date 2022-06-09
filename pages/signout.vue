b12a4bd3bfa4f65cc0fc81f609a73dc6ca936f48
Wisanu <wisanu2308@gmail.com>
2022-06-09 10:59:41 +0700

    update signout

--- pages/signout.vue
+++ pages/signout.vue
@@ -2,11 +2,11 @@
   <div class="flex justify-center">
     <div class="flex flex-col items-center">
       
-      <div class="text-2xl font-bold text-blue-300">ออกจากระบบเรียบร้อย</div>
-      <div class="w-1/5 mx-auto">
+      <div class="text-2xl font-bold text-blue-500 py-10">ออกจากระบบเรียบร้อย</div>
+      <div class="w-3/5 mx-auto">
         <div 
           @click="closeWindow"
-          class="text-xl font-white bg-green-600 px-4 py-2 text-center transition-all duration-300 hover:bg-green-500"
+          class="text-xl text-white bg-green-600 px-4 py-2 text-center rounded-full cursor-pointer transition-all duration-300 hover:bg-green-500"
         >ปิด</div>
       </div>
     </div>
